4f52604f8412ded02cb68fe667734cae8c6ed1c9
sabila220730003-dot <sabila.220730003@mhs.unimal.ac.id>
2026-07-05 18:19:11 +0700

    FILE UPLOAD

---
+++ story_content/triggers.js
@@ -0,0 +1,62 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5pgkQwwfql3":
+        Script1();
+        break;
+      case "6LjhJSfUG5e":
+        Script2();
+        break;
+      case "6UEmDiuwHzD":
+        Script3();
+        break;
+      case "69doobWRk6n":
+        Script4();
+        break;
+      case "5pZnve8M41A":
+        Script5();
+        break;
+      case "5rEOQMyPiSp":
+        Script6();
+        break;
+      case "6or0MlpwL8A":
+        Script7();
+        break;
+      case "6m7B3SiKUSm":
+        Script8();
+        break;
+      case "6fY5vxF7WwJ":
+        Script9();
+        break;
+      case "5XD1NsDZCYN":
+        Script10();
+        break;
+      case "6kElf3965q4":
+        Script11();
+        break;
+      case "6fDKT82COFx":
+        Script12();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
